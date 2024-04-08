e72c8954f432a97275d5df702019591d049bb75e
Mitchell <108231884+ANTIBigBoss@users.noreply.github.com>
2024-04-08 08:04:24 -0400

    Add files via upload

---
+++ MGS1 MC Cheat Trainer/Form1.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace MGS1_MC_Cheat_Trainer
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            pictureBox1 = new PictureBox();
+            Stinger = new Label();
+            label1 = new Label();
+            pictureBox4 = new PictureBox();
+            comboBox1 = new ComboBox();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).BeginInit();
+            SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.BackgroundImage = (Image)resources.GetObject("pictureBox1.BackgroundImage");
+            pictureBox1.BackgroundImageLayout = ImageLayout.Stretch;
+            pictureBox1.Location = new Point(250, 12);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(190, 102);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            // 
+            // Stinger
+            // 
+            Stinger.AutoSize = true;
+            Stinger.BackColor = SystemColors.ActiveCaptionText;
+            Stinger.Font = new Font("Metal Gear", 15.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            Stinger.ForeColor = SystemColors.ButtonFace;
+            Stinger.Location = new Point(252, 90);
+            Stinger.Name = "Stinger";
+            Stinger.Size = new Size(111, 23);
+            Stinger.TabIndex = 1;
+            Stinger.Text = "Stinger";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.BackColor = SystemColors.ActiveCaptionText;
+            label1.Font = new Font("Metal Gear", 15.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label1.ForeColor = SystemColors.ButtonFace;
+            label1.Location = new Point(12, 88);
+            label1.Name = "label1";
+            label1.Size = new Size(91, 23);
+            label1.TabIndex = 5;
+            label1.Text = "Socom";
+            // 
+            // pictureBox4
+            // 
+            pictureBox4.BackgroundImage = (Image)resources.GetObject("pictureBox4.BackgroundImage");
+            pictureBox4.BackgroundImageLayout = ImageLayout.Stretch;
+            pictureBox4.Location = new Point(12, 12);
+            pictureBox4.Name = "pictureBox4";
+            pictureBox4.Size = new Size(190, 102);
+            pictureBox4.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox4.TabIndex = 4;
+            pictureBox4.TabStop = false;
+            // 
+            // comboBox1
+            // 
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Location = new Point(12, 114);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(190, 23);
+            comboBox1.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.Control;
+            BackgroundImage = (Image)resources.GetObject("$this.BackgroundImage");
+            ClientSize = new Size(1166, 706);
+            Controls.Add(comboBox1);
+            Controls.Add(label1);
+            Controls.Add(pictureBox4);
+            Controls.Add(Stinger);
+            Controls.Add(pictureBox1);
+            Name = "Form1";
+            Text = "MGS1 MC Cheat Trainer - ANTIBigBoss - Version 1.0";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private PictureBox pictureBox1;
+        private Label Stinger;
+        private Label label1;
+        private PictureBox pictureBox4;
+        private ComboBox comboBox1;
+    }
+}
---
+++ MGS1 MC Cheat Trainer/Form1.cs
@@ -0,0 +1,10 @@
+namespace MGS1_MC_Cheat_Trainer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
